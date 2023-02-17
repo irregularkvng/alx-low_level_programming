30e711b9dc9f752aa8c8cb10100e9668df210a9c
Peter Ominyi <peter.ominyi@yahoo.com>
2023-02-16 17:09:51 -0800

    hello-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
