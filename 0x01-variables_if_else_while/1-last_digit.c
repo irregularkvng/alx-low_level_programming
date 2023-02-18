cc347dbf6944d888dff16956230dfa219511af86
Peter Ominyi <peter.ominyi@yahoo.com>
2023-02-17 21:01:41 -0800

    print comb c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Determines either greater than 5, is less than 6, or is 0
+ * main - Determines if a number is  greater than 5, is less than 6, or is 0
  *
  * Return: Always 0 (Success)
  */
---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+ /*
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (j != i && j < i)
+			{
+				putchar(j);
+				putchar(i);
+				if (i == 57 && j == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+				i++;
+			}
+			j++
+		}
+		putchar('\n');
+		return (0);
+}
