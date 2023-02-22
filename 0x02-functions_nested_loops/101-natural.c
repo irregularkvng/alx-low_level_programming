b9567a961910139166cd20af863169b521d540f5
Peter Ominyi <peter.ominyi@yahoo.com>
2023-02-21 20:40:24 -0800

    101 natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print sum of all multiples of 3 and 5 up to 1024
+ *
+ * @void: Empty parameter list for main
+ *
+ * Description: prints the sum of all multiples of 3 and 5
+ * up to but not including 1024
+ *
+ * Return: return 0
+ *
+ **/
+
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
