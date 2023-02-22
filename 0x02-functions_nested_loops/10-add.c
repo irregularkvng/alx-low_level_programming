231a2fd5b32eb02f8c984bde1e1227e2e3eb03b8
Peter Ominyi <peter.ominyi@yahoo.com>
2023-02-21 20:26:06 -0800

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two ints
+ * @a: first int to add
+ * @b: second int to add
+ * Return: sum of both ints
+ **/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
