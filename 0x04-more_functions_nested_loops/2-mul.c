1b4a23412d099e0327ec9d66d870639b662b05c1
Peter Ominyi <peter.ominyi@yahoo.com>
2023-02-24 22:12:16 +0000

    multiply_

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
 #include "main.h"
+
+/**
+* mul - multiply two integers
+* @a: store this value
+* @b: multiply to a
+* Return: product of a and b
+*/
 
 int mul(int a, int b)
 {
