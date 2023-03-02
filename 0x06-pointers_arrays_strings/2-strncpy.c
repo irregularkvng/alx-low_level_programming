37c2c664eb636a9e7def4e950e64d5a4ab22f3e1
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-02 04:54:30 +0000

    string concatenation using max number of bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,17 +13,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int str1 = 0;
-	int str1 = 0;
+	int str2 = 0;
 
 	while (src[str1++])
 		str2++;
+		
+	for (str1 = 0; src[str1] && str1 < n; str1++)
+		dest[str1] = src[str1];
 
-		for (str1 = 0; src[str1] && str1 < n; str1++)
-			dest[str1] = src[str1];
+	for (str1 = str2; str1 < n; str1++)
+		dest[str1] = '\0';
 
-		for (str1 = str2; str1 < n; str1++)
-			dest[str1] = '\0';
-
-		return (dest);
+	return (dest);
 }
-
