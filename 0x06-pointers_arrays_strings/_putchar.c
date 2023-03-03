2a5c1ca087bcf7218471ba0bc597a04c58ae0df6
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-02 20:55:46 -0800

    print number

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+* _putchar - it writes the character c to stout
+* &c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and the erroneous is set appropriately.
+*/
+int _putchar(char c)
+{
+        return (write(1, &c,1));
+}
