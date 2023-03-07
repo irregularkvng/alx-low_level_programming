e1778a2a54a82e7a1f0dd3be19bfde53733d1ca1
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-06 17:54:59 -0800

    string search

--- 0x07-pointers_arrays_strings/4-strbrk.c
+++ 0x07-pointers_arrays_strings/4-strbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strpbrk - Searches a string for any of a set of bytes.
+ * _strpbrk - function that searches a string for any of a set of bytes.
  * @s: The string to be searched.
  * @accept: The set of bytes to be searched for.
  *
