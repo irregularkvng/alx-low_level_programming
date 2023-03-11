efa5ad0717292b9538faba568bc04204bb9c8ef6
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-10 17:08:54 -0800

    program that prints the number of parameters

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function that prints the number of arguments passed to it
+ * @argc: areguement count
+ * @argv: arguement vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+			printf("%d\n", i - 1);
+	}
+}
