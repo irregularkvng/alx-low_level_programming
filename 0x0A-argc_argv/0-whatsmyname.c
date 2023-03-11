06de5c16c4d85ac0ef41d99ef1ae36ef87697c9f
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-10 16:37:05 -0800

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+* main - function that prints iys name
+* @i: the first item in array argv
+* @argc: argument count
+* @argv: arguement vector
+* Return: always 0
+*
+*/
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for ( i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
