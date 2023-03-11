383f8f6f0d5fdaaebfdf819fb87a84e0c2229772
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-10 17:35:29 -0800

    function that prints all arguements it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function that prints all arguement it receives
+ * @argc: arguement count
+ * @argv: argument array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("argc = %d\n", argc);
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+
+	return (0);
+}
