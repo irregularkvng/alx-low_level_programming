b76a1a8f3b64c4bb85324174211de05602813761
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-20 15:49:23 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new dog struct.
+ */
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_new - Typedef for struct dog
+ */
+typedef struct dog dog_new;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_new *new_dog(char *name, float age, char *owner);
+void free_dog(dog_new *d);
+
+#endif
