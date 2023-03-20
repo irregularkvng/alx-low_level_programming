cfffba0ff18e23117516c7302405a3bb3fab8744
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-20 15:22:08 -0700

    add numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       which computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
