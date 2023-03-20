d1987801e9b98195b905591f991e2ea2101b4594
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-20 13:59:23 -0700

    object like macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,13 @@
 #ifndef SIZE
+#define SIZE
+
+/*
+ * File: 0-object_like_macro.h
+ *Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ *
+ */
+
 #define SIZE 1024
 
-
-#endif // #ifndef SIZE
+#endif
