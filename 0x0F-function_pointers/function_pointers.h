3a1f67da29fb62b52d16fc2771a481ac4e7ea55a
Peter Ominyi <peter.ominyi@yahoo.com>
2023-03-22 14:06:18 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
