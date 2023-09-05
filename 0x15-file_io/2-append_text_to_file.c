ec199ed27bb9a4d38cd0459731d456f342f1e214
Peter Ominyi <peter.ominyi@yahoo.com>
2023-09-04 20:10:40 -0700

    Speak gently she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ *
+ * @filename: the name of the file
+ * @text_content: the text to append to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, res = -1;
+	ssize_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND)
+		if (fd < 0)
+			return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		res = write(fd, text_content, len);
+		if (res != len)
+			res = -1;
+	}
+	else
+	{
+		res = 1;
+	}
+	close(fd);
+	return (res);
+}
