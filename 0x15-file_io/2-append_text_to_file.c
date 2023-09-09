8783c4f32d377ca7c269d220e8212289ddeaf2bf
Peter Ominyi <peter.ominyi@yahoo.com>
2023-09-09 02:38:37 -0700

    Fixed code and added error handling

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
+
 /**
  * append_text_to_file - appends text at the end of a file
  *
@@ -10,28 +12,29 @@
  *
  * Return: 1 on success, -1 on failure
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, res = -1;
-	ssize_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
+
+	fd = open(filename, O_WRONLY | O_APPEND)
 		if (fd < 0)
 			return (-1);
+
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
-			len++;
+		ssize_t len = strlen(text_content);
+
 		res = write(fd, text_content, len);
-		if (res != len)
-			res = -1;
 	}
 	else
 	{
 		res = 1;
 	}
+
 	close(fd);
-	return (res);
+	return (res == -1 ? -1 : 1);
 }
